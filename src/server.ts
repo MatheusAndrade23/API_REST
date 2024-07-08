59a3c7f9ee2b31286cfc9d345051669178da3071
Matheus Andrade <matheusandrade.ma2003@gmail.com>
2024-07-08 11:25:17 -0300

    Zod for Environment Variables Validation

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import fastify from "fastify";
 import { knex } from "./database";
 
+import { env } from "./env";
+
 const app = fastify();
 
 app.get("/", async (request, reply) => {
   const test = knex;
 });
 
-app.listen({ port: 3000 }).then(() => {
+app.listen({ port: env.PORT }).then(() => {
   console.log("Server is running on port 3000");
 });
